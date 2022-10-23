79ad4a0432f5f150276a33b5605328adb659281b
zulfaaudina11@gmail.com <zulfaaudina11@gmail.com>
2022-10-23 19:19:28 +0700

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,33 @@
-import React from "react";
-
-import Counter from "./components/Counter";
-
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 import './assets/style.css'
 
-const App = () => {
+function App() {
+  const [characters, setCharacters] = useState([])
+
+  useEffect(() =>{
+    const fetchData = async () => {
+      try{
+        const {data} = await axios.get(`https://rickandmortyapi.com/api/character`)
+        setCharacters(data.results)
+      }catch(error){
+        console.error(error);
+      }
+    }
+    fetchData()
+  },[])
+
   return (
-    <>
-      <Counter />
-    </>
+    <div className="App">
+      <div className="results">
+        {characters.map(character => (
+          <div>
+            <img src={character.image} alt={character.name}/>
+            {character.name}
+          </div>
+        ))}
+      </div>
+    </div>
   )
 }
 

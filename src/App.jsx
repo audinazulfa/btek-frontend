b063ab4ba38fa81c62e16d03fdd44e6939d85d0e
zulfaaudina11@gmail.com <zulfaaudina11@gmail.com>
2022-10-26 17:11:38 +0700

    Auth flow login, route profile

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,28 @@
 import React from 'react';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import CharacterDetail from './pages/CharacterDetail';
-import CharacterList from './pages/CharacterList';
+
+import RequireAuth from './components/RequireAuth';
+
 import Home from './pages/Home';
+import Login from './pages/Login';
+import Profile from './pages/Profile';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <Home />,
+    element: <RequireAuth><Home /></RequireAuth>,
   },
   {
-    path: '/characters',
-    element: <CharacterList />,
+    path: '/login',
+    element: <Login />,
   },
   {
-    path: '/characters/:id',
-    element: <CharacterDetail />,
+    path: '/profile',
+    element: <RequireAuth><Profile /></RequireAuth>,
   },
 ]);
 
-function App() {
+export function App() {
   return (
     <RouterProvider router={router} />
   );
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,39 +1,39 @@
-import React from 'react';
+// import React from 'react';
 
-function Counter() {
-  const [counter, setCounter] = React.useState(1);
+// function Counter() {
+//   const [counter, setCounter] = React.useState(1);
 
-  const minus = () => {
-    // console.log('Klik button -');
-    if (counter > 1) {
-      setCounter(counter - 1);
-    } else {
-      // window.alert('Tidak dapat dikurang lagi!');
-    }
-  };
+//   const minus = () => {
+//     // console.log('Klik button -');
+//     if (counter > 1) {
+//       setCounter(counter - 1);
+//     } else {
+//       // window.alert('Tidak dapat dikurang lagi!');
+//     }
+//   };
 
-  const plus = () => {
-    // console.log('Klik button +');
-    if (counter < 9) {
-      setCounter(counter + 1);
-    } else {
-      // window.alert('Tidak dapat ditambah lagi!');
-    }
-  };
+//   const plus = () => {
+//     // console.log('Klik button +');
+//     if (counter < 9) {
+//       setCounter(counter + 1);
+//     } else {
+//       // window.alert('Tidak dapat ditambah lagi!');
+//     }
+//   };
 
-  return (
-    <div className="wrapper">
-      <div className="counter">
-        <div>
-          <button type="button" onClick={minus} className="btn">-</button>
-        </div>
-      </div>
-      <div className="number">{counter}</div>
-      <div>
-        <button type="button" onClick={plus} className="btn">+</button>
-      </div>
-    </div>
-  );
-}
+//   return (
+//     <div className="wrapper">
+//       <div className="counter">
+//         <div>
+//           <button type="button" onClick={minus} className="btn">-</button>
+//         </div>
+//       </div>
+//       <div className="number">{counter}</div>
+//       <div>
+//         <button type="button" onClick={plus} className="btn">+</button>
+//       </div>
+//     </div>
+//   );
+// }
 
-export default Counter;
+// export default Counter;

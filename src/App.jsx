e586f05a58b750af4c997ceaff915d258819b6db
zulfaaudina11@gmail.com <zulfaaudina11@gmail.com>
2022-10-26 23:46:37 +0700

    auth flow register

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import RequireAuth from './components/RequireAuth';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Profile from './pages/Profile';
+import Register from './pages/Register';
 
 const router = createBrowserRouter([
   {
@@ -15,6 +16,10 @@ const router = createBrowserRouter([
   {
     path: '/login',
     element: <Login />,
+  },
+  {
+    path: '/register',
+    element: <Register />,
   },
   {
     path: '/profile',
---
+++ src/pages/Register.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import http from '../helpers/http';
+
+function Register() {
+  const navigate = useNavigate();
+  const register = async (e) => {
+    try {
+      e.preventDefault();
+      const form = {
+        email: e.target.email.value,
+        password: e.target.password.value,
+      };
+      const encoded = new URLSearchParams(form);
+      const { data } = await http().post('/auth/register', encoded.toString());
+      window.localStorage.setItem('token', data.result.token);
+      navigate('/');
+    } catch (err) {
+      window.alert(err.response.data.message);
+    }
+  };
+
+  return (
+    <form onSubmit={register}>
+      Email :
+      <br />
+      <input type="email" name="email" />
+      <br />
+      Password :
+      <input type="password" name="password" />
+      <br />
+      <button type="submit">Go Login</button>
+    </form>
+  );
+}
+
+export default Register;
